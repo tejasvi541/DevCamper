463dfc046140a327a8466ba950a5fc3beb22077c
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-01-01 08:27:27 +0530

    Added Pagination Object

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -45,12 +45,43 @@ exports.getBootcamps = asyncHandler(async (req,res,next) =>{
             query = query.sort("-createdAt")
         }
 
+        // Pagination
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+
+            // to skip certain amount of resources
+        const startIndex = (page-1)*limit;
+
+        const endIndex = page*limit;
+        const total = await Bootcamp.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
+
         // Executing Query
         const bootcamps = await query;
+
+        // Pagination Result
+        const pagination = {};
+
+        if (endIndex<total) {
+            pagination.next = {
+                page : page+1,
+                limit
+            }
+        }
+
+        if(startIndex>0){
+            pagination.prev = {
+                page:page-1,
+                limit
+            }
+        }
 
         res.status(200).json({
             success:true,
             count : bootcamps.length,
+            pagination,
             data:bootcamps
         });
      
