d8be0d913d5c1824646d144e909e614409c46f85
Tejasvi <tejasvi.tanwar541@gmail.com>
2021-01-04 19:35:12 +0530

    Role Authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -20,7 +20,7 @@ const courseRouter = require("./courses");
 const router = express.Router();
 
 // require protect
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource router
 router.use('/:bootcampId/courses', courseRouter);
@@ -33,16 +33,16 @@ router.route("/radius/:zipcode/:distance")
 
 
 router.route("/:id/photo")
-                .put(protect, bootcampPhotoUpload);
+                .put(protect, authorize('publisher', 'admin'),bootcampPhotoUpload);
 
 router.route("/")
         .get(advancedResults(Bootcamp, 'courses') ,getBootcamps)
-        .post(protect, createBootcamp);
+        .post(protect,  authorize('publisher', 'admin'),createBootcamp);
 
 router.route("/:id")
         .get(getBootcamp)
-        .put(protect, updateBootcamp)
-        .delete(protect, deleteBootcamp);
+        .put(protect,  authorize('publisher', 'admin'),updateBootcamp)
+        .delete(protect,  authorize('publisher', 'admin'),deleteBootcamp);
         
 
 
